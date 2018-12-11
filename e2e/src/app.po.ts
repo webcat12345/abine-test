f4eb813179cfee0ac2ba8f142595c662bed0716e
webcat12345 <webcat91@gmail.com>
2018-12-11 09:49:23 -0800

    init - angular-cli.7

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('abine-root h1')).getText();
+  }
+}
